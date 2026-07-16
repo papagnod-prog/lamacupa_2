7e58620015b5f132e4fe8fd21bea0679484b7217
Simply App <simplyapp@Host-005.homenet.telecomitalia.it>
2026-07-16 20:45:40 +0200

    Aggiornato percorso DOCROOT assoluto in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 # Azione di distribuzione per Plesk (Git → "Attiva azioni di distribuzione aggiuntive")
 # Copia tema e plugin nelle cartelle di WordPress dopo ogni push.
-# Adatta DOCROOT al percorso reale del sito.
-DOCROOT="$HOME/httpdocs"
+
+# PERCORSO ASSOLUTO DEL TUO DOMINIO SU PLESK
+DOCROOT="/var/www/vhosts/dazzling-meitner.136-144-244-35.plesk.page/httpdocs"
+
+# Copia in modo incrementale ed elimina i file rimossi in locale
 rsync -a --delete lamacupa-theme/    "$DOCROOT/wp-content/themes/lamacupa-theme/"
 rsync -a --delete lamacupa-pannello/ "$DOCROOT/wp-content/plugins/lamacupa-pannello/"
